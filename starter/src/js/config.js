810302e950fb512d57111c852fed081d3bf849f8
Halke99 <alperinan078@gmail.com>
2023-02-24 12:59:56 +0100

    Initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3ec5c753-1901-498e-8af8-4df04b5ef1b4';
+export const MODAL_CLOSE_SEC = 2.5;
