64458c18b350f215d1c7e819fb9ca31f149c7596
Alper Inan <92399862+Halke99@users.noreply.github.com>
2023-02-27 17:22:46 +0100

    Update config.js

--- starter/src/js/config.js
+++ starter/src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '3ec5c753-1901-498e-8af8-4df04b5ef1b4';
